91fa5f6cc5d2b162f3d0aa871bda4172c59a0dd4
Piotr Goliasz <piotr.goliasz@post.pl>
2016-11-21 10:06:32 +0000

    2.0.9

--- privela.php
+++ privela.php
@@ -33,7 +33,7 @@ class Privela extends Module
         $this->module_key = '1d0f87f5da56b0d9d6417743f41f007a';
         $this->name = 'privela';
         $this->tab = 'advertising_marketing';
-        $this->version = '2.0.8';
+        $this->version = '2.0.9';
         $this->author = 'RBox24';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = array('min' => '1.6', 'max' => '1.7');
